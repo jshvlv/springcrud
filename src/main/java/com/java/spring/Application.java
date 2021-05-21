9f762eb356227f006d5e20367339cd8acfbcd36e
jshvlv <dj-shev-elev@mail.ru>
2021-05-21 10:17:21 +0300

    modified application

--- src/main/java/com/java/spring/Application.java
+++ src/main/java/com/java/spring/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        //To start our application
         SpringApplication.run(Application.class, args);
     }
 }
